8d5d931fb477eaaa3daddc7c7fa9140ac69b3177
JayPatel-2023 <mr.jaypatel2002@gmail.com>
2023-10-13 18:40:45 +0530

    Usercontroller and Userresource created

--- react/src/views/Users.jsx
+++ react/src/views/Users.jsx
@@ -1,9 +1,31 @@
 //in this file we create the Users page view of the our site
+import {useEffect, useState} from "react";
+import axiosClient from "../axios-client.js";
+import {Link} from "react-router-dom";
+import {useStateContext} from "../contexts/ContextProvider.jsx";
 
 export default function Users(){
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(()=>{
+        getUsers()
+    },[])
+
+    const getUsers = () => {
+        setLoading(true)
+        axiosClient.get('/users')
+            .then(({data})=>{
+                setLoading(false)
+                console.log(data);
+            }).catch(()=>{
+                setLoading(false);
+            })
+    }
+
     return (
         <div>
-            Users
+            <Link></Link>
         </div>
     )
 }
