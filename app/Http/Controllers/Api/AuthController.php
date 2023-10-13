4a4b0651bfa2b9e3fa30bf5a11f35b2786cc0669
JayPatel-2023 <mr.jaypatel2002@gmail.com>
2023-10-13 05:38:58 +0530

    setup routes for backed to handle the data

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,21 +3,26 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\SignupRequest;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     //when payload send using axios api it reach here in backend part(server) 
 
-    public function login(){
+    //created LoginRequest class using php artisan make:request LoginRequest
+    public function login(LoginRequest $request){
 
     }
 
-    public function signup(){
+    //created SignupRequest
+    public function signup(SignupRequest $request){
 
     }
 
-    public function logout(){
+    //use built-in class Request
+    public function logout(Request $request){
         
     }
 }
